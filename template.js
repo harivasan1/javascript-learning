c240c56edd8844c27d8cbb6cd3fb9e28baa51d4e
harivasan1 <kharivasan@gamil.com>
2026-06-28 17:39:45 +0530

    hari

--- template.js
+++ template.js
@@ -6,4 +6,103 @@ let position=" full stack develpoer "
 let jobexaplin = 'also the eniter all team head manger'
 console.log(`the company manger is ${myname} and he is the ${position} and ${jobexaplin} `)
 
-// inside the type in the 
+// inside the type in the string using 
+//-----------------------------------------------------------------------------------------------------------------
+//  the expression used for numbers and  (*,+,-,%)  this are do it inide these moderen method 
+// example = 1
+
+let price = 1000
+let gst = 30
+let distcount = 10
+let total = `the total price is the ${price + gst - distcount} welcome the  shop more offer  and next vist you truth  comapny  `
+console.log(total)
+
+// example =2
+
+let  pencount  =  100
+let penname = "unomax"
+let penprice =10
+let distcount1 = 20
+let gst1 = 10
+let total1= `the pen comapny name is the ${penname} and the i buy the pen countis the ${pencount} and each pen price is the ${penprice} the total prices is the ${penprice * pencount- distcount1 +gst1}  and thank you for buying and enjoy the studying  `
+console.log(total1)
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+ // this used in Multi-Line Strings 
+ // example -1 
+
+ let container = `apple 
+  mango 
+  watermelon `
+ 
+console.log(container)
+
+
+// Email Message -example -2
+console.log(`this the example for email `)
+
+let user ="hari"
+
+let email =`
+dear ${user}
+
+your account has been created succsefuly .
+
+
+thank you,
+hari`
+
+console.log(email)
+
+console .log(`--------------------------------------`)
+
+console.log(`Invoice System`)
+
+let customer = "iron man"
+let phonenumber= 5442233355333
+let tshirt = 400 
+let note = 300 
+let pen = 60
+var total3 =` ${tshirt * 5 + note* 10+ pen* 20} `
+var total4 = `  welcome to all and this store  happy to pruchase 
+
+             the customer name = ${customer}
+            
+             phone number = ${phonenumber}
+                                               per t-shrit price = ${tshirt}
+                                                 total t-shrit purchase = 5  
+              total t-shirt Rate = ${tshirt*5}
+                                                  per note price = ${note}
+                                                 total note purchase = 10
+            total note cost = ${note * 10} 
+                                                    per pen price = ${pen}
+                                                    total pen purchase = 20
+            total pen purcahse =${pen* 20}
+              total cost = ${total3}
+              
+              
+              Thank you for shopping! `
+console.log(total4) 
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+//  this the html style used 
+ 
+let client  = "Tony";
+
+const html = `
+    <section>
+        <h1>Hello ${client}</h1>
+        <button>Start Learning</button>
+    </section>
+`;
+
+console.log(html)
+
+
+// --------------------------------------------------------------------------------------------------------------
+
+// next will contiue-- fuction call , loop and condtional  and nested , 
+
+
+
